8fa33a24a0235330d95c0d93e39207464283ed94
anna <614767386@qq.com>
2024-05-28 09:13:25 +0800

    样式修改

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,23 @@ export default function IndexPage() {
         </div>
         <Image src={web3tools} width={450} alt={""}/>
       </div>
-      <div className="grid items-center justify-center gap-1 h-screen">
-          <div>
-            <div className="text-3xl font-black text-white">WE ONLY WORK ON WEB3</div>
-              <div className="text-3xl font-black text-[#F7B34E] ">AND CREATE TOOLS FOR</div>
-            <div className="text-3xl font-black text-[#F7B34E] ">BLOCK CHAIN</div>
+      <div className="flex flex-col items-center justify-center gap-40 h-screen text-6xl font-black text-white">
+          <div className="flex flex-col items-center justify-center ">
+            <div>WE ONLY WORK ON <span className="text-[#F7B34E] ">WEB3</span></div>
+            <div>AND CREATE TOOLS FOR</div>
+            <div className="text-[#F7B34E] ">BLOCK CHAIN</div>
           </div>
           <div className="flex items-center justify-center">
-            <div className="mr-10">OUR SPONSOR</div>
-            <div className="flex justify-center bg-[#212121] px-5 py-1 rounded-lg"><Image src={dmc} width={150} alt=""/></div>
-          </div>
-          <div className="mt-100">
-            <div className="text-3xl font-black text-[#F7B34E]"> TOOLS DAO A GROUP OF DEVELOPERS WHO</div>
-            <div className="text-3xl font-black text-[#F7B34E] ">LOVE WEB3 TECHNOLOGY AND EMBRACE</div>
-            <div className="text-3xl font-black text-[#F7B34E] ">WEB3, SO THAT WEB3 CAN BRING MORE</div>
-            <div className="text-3xl font-black text-[#F7B34E] ">BENEFITS AND CONVENIENCE TO PEOPLE</div>
+            <div className="text-3xl mr-10">OUR SPONSOR</div>
+            <div className="flex justify-center bg-[#212121] px-5 py-3 rounded-lg"><Image src={dmc} width={200} alt=""/></div>
           </div>
       </div>
+      <div className="flex flex-col items-center justify-center gap-2 h-screen text-3xl font-black text-[#F7B34E]">
+            <div> TOOLS DAO A GROUP OF DEVELOPERS WHO</div>
+            <div>LOVE WEB3 TECHNOLOGY AND EMBRACE</div>
+            <div>WEB3, SO THAT WEB3 CAN BRING MORE</div>
+            <div>BENEFITS AND CONVENIENCE TO PEOPLE</div>
+          </div>
     </section>
   )
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -59,7 +59,7 @@ export function SiteHeader() {
         <AppProvider value={account}>
           <MainNav />
           <div className="flex flex-1 items-center justify-end space-x-4">
-          <Image src={dmc} width={100} alt={""} />
+          <Image src={dmc} width={60} alt={""} />
 
             <nav className="flex items-center space-x-1">
               {/* <ThemeToggle /> */}
